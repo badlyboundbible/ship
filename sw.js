82c120f457110a4ed030258d823823544629bb4a
badlyboundbible <dmillar1919@gmail.com>
2024-09-14 13:46:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
+self.addEventListener('install', (event) => {
+    event.waitUntil(
         caches.open('ship-rating-cache').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/manifest.json',
-                '/sw.js',
                 '/icon-192x192.png',
                 '/icon-512x512.png'
             ]);
@@ -13,10 +12,10 @@ self.addEventListener('install', (e) => {
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
